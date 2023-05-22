607dcad2546c8a0e3f50d915cc89e8a2472ed78c
CGRITHY <ahpea88@gmail.com>
2023-05-22 13:26:58 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Import the CSS file
+
+function App() {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    fetch('/members')
+      .then(response => response.json())
+      .then(data => {
+        setMembers(data.members);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Members:</h1>
+      <table className="members-table"> {/* Apply CSS class */}
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>School</th>
+          </tr>
+        </thead>
+        <tbody>
+          {members.map((member, index) => (
+            <tr key={index}>
+              <td>{member.name}</td>
+              <td>{member.gender}</td>
+              <td>{member.school}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
